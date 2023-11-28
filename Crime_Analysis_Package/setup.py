21655c58d55d32f7195a8e722b6725225bc05ca3
Aquib Hussain Mohammed <109718972+Aquibgitt@users.noreply.github.com>
2023-11-28 15:54:31 -0500

    Add files via upload
    Uploading All the Required Packages in the Modules.

---
+++ Crime_Analysis_Package/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='Crime_Analysis_Package',
+      version=0.1,
+      description='Project 3 M11',
+      author='Aquib Hussain',
+      packages=['Data_Summary','The_EDA','The_inference'],
+      zip_safe=False)
